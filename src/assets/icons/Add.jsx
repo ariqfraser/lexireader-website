d6ef3e9d07cac2b97f71e297b1f140142d23e86b
Ariq Fraser <ariqfraser@gmail.com>
2022-03-28 00:02:20 +0100

    fixed firebase rewrite desitnation to fix 404 bug

---
+++ src/assets/icons/Add.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Add = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="45.197"
+            height="45.197"
+            viewBox="0 0 45.197 45.197"
+        >
+            <path
+                d="M51.348,28.75H34.377V11.779a2.814,2.814,0,1,0-5.628,0V28.75H11.779a2.814,2.814,0,0,0,0,5.628H28.75V51.348a2.814,2.814,0,1,0,5.628,0V34.377H51.348a2.814,2.814,0,1,0,0-5.628Z"
+                transform="translate(-8.965 -8.965)"
+                fill="#fafafa"
+            />
+        </svg>
+    );
+};
