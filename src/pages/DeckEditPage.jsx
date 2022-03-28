c7fb60d3c5cbf41fc6101066f6a12205ce668fa0
Ariq Fraser <ariqfraser@gmail.com>
2022-03-28 23:48:33 +0100

    deck edit functionality. TODO: delete deck, delete card.

--- src/pages/DeckEditPage.jsx
+++ src/pages/DeckEditPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment, useRef } from 'react';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import PageTemplate from '../components/PageTemplate';
 import { Box, SubTitle, Footer, Title, StatText } from '../components/Box';
@@ -10,16 +10,19 @@ import {
     doc,
     getDocs,
     collectionGroup,
+    collection,
+    setDoc,
+    addDoc,
 } from 'firebase/firestore';
 import { db } from '../lib/init-firebase';
 import { getAuthState } from '../lib/authState';
 
-const DeckSettingsBox = styled('div')(({ col }) => ({
+const DeckSettingsBox = styled('form')(({ col }) => ({
     backgroundColor: 'var(--w)',
     padding: 16,
     display: 'grid',
     gridTemplateColumns: col ? `repeat(${col}, 1fr)` : '1fr 1fr',
-    gap: 2,
+    gap: 4,
     position: 'relative',
     borderRadius: 16,
     color: 'var(--b)',
@@ -30,11 +33,22 @@ const DeckSettingsBox = styled('div')(({ col }) => ({
         color: 'var(--b)',
         fontWeight: 'bold',
         fontSize: 12,
-        gridColumn: `span ${col || 2}`,
+        gridColumn: col ? `span ${col}` : `span 2`,
         padding: '8px 0',
     },
     filter: 'var(--shadow)',
     gridColumn: 'span 2',
+}));
+
+const CardList = styled('div')(() => ({
+    display: 'grid',
+    backgroundColor: 'var(--w)',
+    gridTemplateColumns: '1fr 1fr 50px',
+    gridColumn: 'span 2',
+    gap: 4,
+    padding: 8,
+    borderRadius: 8,
+    filter: 'var(--shadow)',
 }));
 
 const ActionButton = styled('button')(({ bg }) => ({
@@ -65,6 +79,73 @@ const Spacer = styled('div')(() => ({
     margin: '4px 0',
 }));
 
+const FormSaveButton = styled('button')(() => ({
+    gridColumn: 'span 2',
+    backgroundColor: 'var(--g)',
+    border: 'none',
+    cursor: 'pointer',
+    borderRadius: 16,
+    padding: 4,
+    marginTop: 4,
+    fontWeight: 900,
+}));
+
+const FormDeleteButton = styled('button')(() => ({
+    backgroundColor: 'var(--r)',
+    border: 'none',
+    cursor: 'pointer',
+    borderRadius: 16,
+    padding: 4,
+    marginTop: 4,
+    fontWeight: 900,
+}));
+
+const EditCardModal = (props) => {
+    const Overlay = styled('div')(() => ({
+        position: 'fixed',
+        width: '100vh',
+        height: '100vh',
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        zIndex: 12,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 0,
+        left: 0,
+    }));
+
+    const Modal = styled('form')(() => ({
+        width: '80vw',
+        padding: 16,
+        backgroundColor: 'var(--w)',
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr',
+        gap: 16,
+        '&>button': {
+            gridColumn: 'span 1',
+        },
+        position: 'relative',
+        borderRadius: 16,
+        '&>#closeBtn': {
+            textTransform: 'uppercase',
+            backgroundColor: 'transparent',
+            border: '2px solid var(--r)',
+            cursor: 'pointer',
+            borderRadius: 16,
+            padding: 4,
+            marginTop: 4,
+            fontWeight: 900,
+            gridColumnStart: 3,
+        },
+    }));
+
+    return (
+        <Overlay>
+            <Modal onSubmit={(e) => e.preventDefault()}>{props.children}</Modal>
+        </Overlay>
+    );
+};
+
 const DeckEditPage = () => {
     const [deckRefParam] = useState(useParams()['deckRef']);
     const [title, setTitle] = useState('');
@@ -79,6 +160,9 @@ const DeckEditPage = () => {
 
     if (!userState) navigate('/');
 
+    const formSaveRef = useRef();
+    const formAddRef = useRef();
+
     useEffect(() => {
         // check if creating a new deck
         if (!isNew.get('isNew')) {
@@ -87,7 +171,7 @@ const DeckEditPage = () => {
 
             async function getDeckData() {
                 const deckRef = doc(db, 'decks', deckRefParam);
-
+                const cardsRef = collection(deckRef, 'cards');
                 await getDoc(deckRef)
                     .then((snap) => {
                         // validation
@@ -101,35 +185,30 @@ const DeckEditPage = () => {
                         // process data
                         // console.log(data); // show data
                         setTitle(data.title);
-                        const cardsRef = query(
-                            collectionGroup(db, 'cards'),
-                            where('deckRef', '==', deckRefParam)
-                        );
-                        getDocs(cardsRef)
+                        const cardsQuery = query(cardsRef);
+                        getDocs(cardsQuery)
                             .then((data) => {
-                                data.docs.forEach((doc) => {
-                                    const card = doc.data();
-                                    setCards(() => [
-                                        ...cards,
-                                        {
-                                            sideA: card['sideA'],
-                                            sideB: card['sideB'],
-                                            correctCount: card['correctCount'],
-                                            failCount: card['failCount'],
-                                            id: doc.id,
-                                        },
-                                    ]);
-
+                                console.log(data);
+                                return data.docs.map((v) => {
                                     setPracticeCount(
                                         practiceCount +
-                                            doc.data()['correctCount'] +
-                                            doc.data()['failCount']
+                                            v.data()['correctCount'] +
+                                            v.data()['failCount']
                                     );
+                                    return {
+                                        sideA: v.data()['sideA'],
+                                        sideB: v.data()['sideB'],
+                                        correctCount: v.data()['correctCount'],
+                                        failCount: v.data()['failCount'],
+                                        id: v.id,
+                                    };
                                 });
                             })
-                            .then(() => setCardCount(cards.length));
+                            .then((cardArr) => {
+                                setCards(cardArr);
+                                setCardCount(cardArr.length);
+                            });
                     })
-
                     .catch((err) => {
                         switch (err.message) {
                             case '469':
@@ -144,60 +223,186 @@ const DeckEditPage = () => {
                         }
                     });
             }
+
             if (userState) getDeckData();
         }
     }, [userState]);
 
-    function handleSaveChanges() {
-        setMode('view');
+    useEffect(() => {
+        async function saveNewTitle(e) {
+            e.preventDefault();
+            const newTitle = {
+                title: formSaveRef.current['title']['value'],
+            };
+            setTitle(newTitle.title);
+
+            if (!isNew.get('isNew')) {
+                const docRef = doc(db, 'decks', deckRefParam);
+                await setDoc(docRef, newTitle, { merge: true });
+            }
+        }
+
+        async function addCard(e) {
+            e.preventDefault();
+
+            const newCard = {
+                correctCount: 0,
+                deckRef: deckRefParam,
+                failCount: 0,
+                sideA: 'aa',
+                sideB: 'bb',
+            };
+            if (!isNew.get('isNew')) {
+                const docRef = doc(db, 'decks', deckRefParam);
+                const cardsRef = collection(docRef, 'cards');
+                await addDoc(cardsRef, newCard);
+            }
+        }
+
+        if (formSaveRef.current)
+            formSaveRef.current.addEventListener('submit', saveNewTitle, true);
+
+        if (formAddRef.current)
+            formAddRef.current.addEventListener('submit', addCard, true);
+    }, [mode]);
+
+    const [modal, setModal] = useState(false);
+    const aRef = useRef();
+    const bRef = useRef();
+    const idRef = useRef();
+    const arrIndexRef = useRef();
+    function handleDeleteCard(e) {
+        e.preventDefault();
     }
-    function handleCancelChanges() {
-        setMode('view');
+
+    async function updateCard(e) {
+        const aVal = aRef.current.value;
+        const bVal = bRef.current.value;
+        const idVal = idRef.current.value;
+
+        // update db
+        const docRef = doc(db, 'decks', deckRefParam);
+        const cardRef = doc(docRef, 'cards', idVal);
+        await setDoc(
+            cardRef,
+            {
+                sideA: aVal,
+                sideB: bVal,
+            },
+            { merge: true }
+        );
+
+        // update cards state to resembe db without another query
+        const newData = {
+            sideA: aVal,
+            sideB: bVal,
+            correctCount: cards[arrIndexRef.current.value]['correctCount'],
+            failCount: cards[arrIndexRef.current.value]['failCount'],
+            id: idVal,
+        };
+        let cardArr = cards;
+        cardArr[arrIndexRef.current.value] = newData;
+        console.log('newData', '=', newData);
+        console.log('cardArr', '=', cardArr);
+    }
+
+    async function showModal(sideA, sideB, cardID, arrIndex) {
+        await setModal(true);
+        aRef.current.value = sideA;
+        bRef.current.value = sideB;
+        idRef.current.value = cardID;
+        arrIndexRef.current.value = arrIndex;
     }
     return (
         <>
             {/* {JSON.stringify(cards)} */}
+
             <PageTemplate page={'flashcards'}>
+                {modal && (
+                    <EditCardModal>
+                        <span>SideA</span>
+                        <span>SideB</span>
+                        <FormSaveButton onClick={() => updateCard()}>
+                            SAVE
+                        </FormSaveButton>
+                        <input type="text" ref={aRef} />
+                        <input type="text" ref={bRef} />
+                        <input type="hidden" ref={idRef} />
+                        <input type="hidden" ref={arrIndexRef} />
+                        <FormDeleteButton onClick={(e) => handleDeleteCard(e)}>
+                            DELETE
+                        </FormDeleteButton>
+                        <button
+                            id={'closeBtn'}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setModal(false);
+                            }}
+                        >
+                            close
+                        </button>
+                    </EditCardModal>
+                )}
                 {mode === 'edit' && (
                     <>
-                        <DeckSettingsBox>
+                        <DeckSettingsBox ref={formSaveRef}>
                             <small>Deck Settings</small>
                             deck title
-                            <input type="text" defaultValue={title} />
-                            deck color
-                            <div>buttons go here</div>
-                            <small>delete deck</small>
-                            <input type="text" placeholder="TYPE DECK TITLE" />
-                            <button>DELETE DECK</button>
+                            <input
+                                type="text"
+                                defaultValue={title}
+                                name="title"
+                            />
+                            {/* deck color
+                                <div>buttons go here</div> */}
+                            <FormSaveButton>SAVE</FormSaveButton>
                         </DeckSettingsBox>
                         <DeckSettingsBox>
+                            <small>delete deck</small>
+
+                            <input type="text" placeholder="TYPE DECK TITLE" />
+
+                            <FormDeleteButton>DELETE DECK</FormDeleteButton>
+                        </DeckSettingsBox>
+                        <DeckSettingsBox ref={formAddRef}>
                             <small>ADD CARD</small>
                             Side A (e.g. a word)
                             <input type="text" />
                             Side B (e.g. the meaning)
                             <input type="text" />
-                            <button style={{ gridColumn: 'span 2' }}>
-                                Add Card
-                            </button>
+                            <FormSaveButton>Add Card</FormSaveButton>
                         </DeckSettingsBox>
                         <DeckSettingsBox col={3}>
                             <small>cards in the deck</small>
-
-                            <input type="text" />
-                            <input type="text" />
-                            <button>delete card</button>
                         </DeckSettingsBox>
+                        <CardList>
+                            {cards.map((v, i) => {
+                                console.log(v);
+                                return (
+                                    <Fragment>
+                                        <span key={'a' + i}>{v.sideA}</span>
+                                        <span key={'b' + i}>{v.sideB}</span>
+                                        <button
+                                            onClick={() =>
+                                                showModal(
+                                                    v.sideA,
+                                                    v.sideB,
+                                                    v.id,
+                                                    i
+                                                )
+                                            }
+                                        >
+                                            Edit
+                                        </button>
+                                        <Spacer />
+                                    </Fragment>
+                                );
+                            })}
+                        </CardList>
+
                         <div style={{ padding: 100 }}></div>
                         <ActionWrapper>
-                            <ActionButton
-                                onClick={() => handleCancelChanges()}
-                                bg="red"
-                            >
-                                cancel
-                            </ActionButton>
-                            <ActionButton onClick={() => handleSaveChanges()}>
-                                Save
-                            </ActionButton>
+                            <ActionButton>done</ActionButton>
                         </ActionWrapper>
                     </>
                 )}
